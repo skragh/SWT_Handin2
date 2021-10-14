03695bf4422765051efb02a0412e8549088d4231
au619496 <201804204@post.au.dk>
2021-10-14 12:32:08 +0200

    Small changes to ChargeControl Test. Added state macine for ChargeControl

--- LadeskabLibrary.Tests/ChargeControlTest.cs
+++ LadeskabLibrary.Tests/ChargeControlTest.cs
@@ -20,7 +20,7 @@ namespace LadeskabLibrary.Tests
         public void Setup()
         {
             _usbSource = Substitute.For<IUsbCharger>();
-            _uut = new IChargeControl(_usbSource);
+            _uut = new ChargeControl(_usbSource);
 
             _uut.ChargingStateEvent += (o, args) =>
             {
@@ -43,7 +43,14 @@ namespace LadeskabLibrary.Tests
             _usbSource.Received().StartCharge();
         }
 
-        //Should this be possible, when StartCharge() is not activated?
+        [Test]
+        public void StartCharge_PhoneNotConnected_CorrectStateSent()
+        {
+            _usbSource.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs { Current = 0 });
+            Assert.That(_receivedEventArgs._chargingState, Is.EqualTo(ChargingState.DISCONNECTED));
+        }
+
+        //Should this be possible, when StartCharge() from StatonControl is not activated?
         [Test]
         public void OnNewCurrentEvent_PhoneNotConnected_EventFired()
         {
@@ -53,11 +60,7 @@ namespace LadeskabLibrary.Tests
             Assert.That(_receivedEventArgs, Is.Not.Null);
         }
 
-        [Test]
-        public void StartCharge_PhoneNotConnected_CorrectStateSent()
-        {
-            _usbSource.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs { Current = 0 });
-            Assert.That(_receivedEventArgs._chargingState, Is.EqualTo(ChargingState.DISCONNECTED));
-        }
+        //OnNewCurrent
+
     }
 }
