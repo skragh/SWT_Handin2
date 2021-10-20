b1c5a5f7c663fddd682f0be1c63710be8588928f
Sigurd Skov Jensen <Sigurd.skov@hotmail.com>
2021-10-20 13:03:54 +0200

    Opdateret klassediagram, succesfuld konsolprinttest

---
+++ LadeskabLibrary.Tests/DisplayTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeskabLibrary.Tests
+{
+    class DisplayTest
+    {
+        private DisplayImplementation uut;
+
+        [SetUp]
+        public void setup()
+        {
+            uut = new DisplayImplementation();
+        }
+
+        [TestCase("Charging")]
+        [TestCase("Fully Charged")]
+        [TestCase("Disconnected")]
+        [TestCase("Overload Error - Too much power being transferred - Disconnecting")]
+        [TestCase("Error exception. Internal charging information is invalid")]
+        [Test]
+        public void DisplayValidChargeMessages_IgnoringStationMessage_AssertThatMessageSentToConsole(string chargeMessage)
+        {
+            //Arrange - setup stringwriter
+            System.IO.StringWriter output = new System.IO.StringWriter(); //Oprettet en stringwriter
+            System.Console.SetOut(output); //i stedet for hvad der normalt skrives til out, skrives der til stringWriterForTest
+
+            //Act - Kør displaymessage
+            uut.ChargeMessage = chargeMessage;
+            uut.print(); //stringwriter -> stringbuilder -> hvad er der i
+            //I stedet for at skrive til konsollen, som den normalt ville, skrives der til stringWriterForTest
+
+            //Assert - hvad er der i stringbuilderen?
+            //Nu er det der blev udskrevet skrevet til en string
+            Assert.That(output.ToString().Replace("\r", string.Empty).Replace("\n", string.Empty), Is.EqualTo(uut.ChargingMessageDeclaration.Trim() + chargeMessage.Trim() + uut.StationMessageDeclaration.Trim())); //Er det der kom ud, det samme som message?
+        }
+
+        [TestCase("Station initialized")]
+        [TestCase("Not empty")]
+        [TestCase("")]
+        [TestCase("\r\n")]
+        [TestCase("Tilslutningsfejl")]
+        public void DisplayValidStationMessage_IgnoringChargingMessage_AssertThatMessageSentToConsole(string stationMessage)
+        {
+            //Arrange - setup stringwriter
+            System.IO.StringWriter output = new System.IO.StringWriter(); //Oprettet en stringwriter
+            System.Console.SetOut(output); //i stedet for hvad der normalt skrives til out, skrives der til stringWriterForTest
+
+            //Act - Kør displaymessage
+            uut.StationMessage = stationMessage;
+            uut.print(); //stringwriter -> stringbuilder -> hvad er der i
+            //I stedet for at skrive til konsollen, som den normalt ville, skrives der til stringWriterForTest
+
+            //Assert - hvad er der i stringbuilderen?
+            //Nu er det der blev udskrevet skrevet til en string
+            Assert.That(output.ToString().Replace("\r", string.Empty).Replace("\n", string.Empty), Is.EqualTo(uut.ChargingMessageDeclaration.Trim() + uut.StationMessageDeclaration.Trim() + uut.StationMessage.Trim())); //Er det der kom ud, det samme som message?
+        }
+    }
+}
---
+++ LadeskabLibrary/DisplayImplementation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeskabLibrary
+{
+    public class DisplayImplementation : IDisplay
+    {
+        public DisplayImplementation()
+        {
+            ChargingMessageDeclaration = "CHARGING INFORMATION - Display information for customer";
+            StationMessageDeclaration = "STATION INFORMATION - Display information for technician";
+        }
+        public void DisplayChargeMessage(object sender, ChargingStateEventArgs e)
+        {
+            switch (e._chargingState)
+            {
+                case ChargingState.CHARGING:
+                    ChargeMessage = "Charging";
+                    break;
+                case ChargingState.FULL:
+                    ChargeMessage = "Fully Charged";
+                    break;
+                case ChargingState.DISCONNECTED:
+                    ChargeMessage = "Disconnected";
+                    break;
+                case ChargingState.OVERLOAD:
+                    ChargeMessage = "Overload Error - Too much power being transferred - Disconnecting";
+                    break;
+                default:
+                    ChargeMessage = "Error exception. Internal charging information is invalid";
+                    break;
+            }
+        }
+
+        public void DisplayStationMessage(object sender, StationControl.StationMessageEventArgs e)
+        {
+            StationMessage = e.message;
+            print();
+        }
+
+        public void print()
+        {
+            printChargingMessage();
+            printStationMessage();
+        }
+
+        private void printChargingMessage()
+        {
+            Console.WriteLine(ChargingMessageDeclaration);
+            Console.WriteLine(ChargeMessage);
+        }
+        private void printStationMessage()
+        {
+            Console.WriteLine(StationMessageDeclaration);
+            Console.WriteLine(StationMessage);
+        }
+
+        public string ChargingMessageDeclaration { get; private set; }
+        public string StationMessageDeclaration { get; private set; }
+        public string ChargeMessage { get; set; }
+        public string StationMessage { get; set; }
+
+
+        //public void DisplayChargeMessage(string message)
+        //{
+        //    switch (message)
+        //    {
+        //        case "currently charging":
+        //            Console.WriteLine(message);
+        //            break;
+        //        case "fully charged":
+        //            Console.WriteLine(message);
+        //            break;
+        //        case "no connection":
+        //            Console.WriteLine(message);
+        //            break;
+        //        default:
+        //            Console.WriteLine("Error exception. Charging information is invalid. Noget er gået galt, eks. kortslutning. Opladning stoppes straks");
+        //            break;
+        //    }
+        //}
+    }
+}
