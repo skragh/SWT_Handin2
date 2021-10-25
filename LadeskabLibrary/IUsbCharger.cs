35f13b9ed255a60d528b73ff821ee214410e6598
Sigurd Skov Jensen <Sigurd.skov@hotmail.com>
2021-10-25 20:53:06 +0200

    Fjernet irrelevant USBCharger

--- LadeskabLibrary/IUsbCharger.cs
+++ LadeskabLibrary/IUsbCharger.cs
@@ -25,22 +25,22 @@ namespace LadeskabLibrary
         void StopCharge();
     }
 
-    public class UsbCharger : IUsbCharger
-    {
-        public double CurrentValue => throw new NotImplementedException();
+    //public class UsbCharger : IUsbCharger
+    //{
+    //    public double CurrentValue => throw new NotImplementedException();
 
-        public bool Connected => throw new NotImplementedException();
+    //    public bool Connected => throw new NotImplementedException();
 
-        public event EventHandler<CurrentEventArgs> CurrentValueEvent;
+    //    public event EventHandler<CurrentEventArgs> CurrentValueEvent;
 
-        public void StartCharge()
-        {
-            throw new NotImplementedException();
-        }
+    //    public void StartCharge()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public void StopCharge()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public void StopCharge()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
